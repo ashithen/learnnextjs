64b225c47aab0ee9c0d82ce49a3bfdd50c372632
ashith <ashien987@gmail.com>
2025-03-17 17:44:04 -0400

    Added new fonts

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,44 +5,33 @@ import { auth, signIn, signOut } from "@/auth";
 
 const Navbar = async () => {
   const session = await auth();
+  
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
+        
         <div className="flex items-center gap-5 text-black">
-          {session && session?.user ? (
+          {session?.user ? (
             <>
-              <Link href="/startup/create">
-                <span>Create</span>
-              </Link>
-              <form action={async () => {
-                "use server"
-                await signOut();
-              }
-              }><button>
-                  Logout
-                </button>
+              <Link href="/startup/create">Create</Link>
+              <form action={async () => { "use server"; await signOut(); }}>
+                <button>Logout</button>
               </form>
-
-              <Link href={'/user/${session?.id}'}>
-                <span>{session?.user.name}</span>
+              <Link href={`/user/${session?.id}`}>
+                {session.user.name}
               </Link>
-
             </>
           ) : (
-            <form action={async () => {
-              "use server";
-              await signIn('github');
-            }}><button type="submit">Login with Github
-
-              </button></form>
-
+            <form action={async () => { "use server"; await signIn('github'); }}>
+              <button type="submit">Login with Github</button>
+            </form>
           )}
         </div>
       </nav>
-    </header >
+    </header>
   )
 }
 
